25ae50a225a6f21a4e3ce156c2eb459bc3a7e80e
kkchuchu <kkchuchualbert@gmail.com>
2017-01-04 16:14:42 +0800

    add reference for git-story.py

--- git-story.py
+++ git-story.py
@@ -4,6 +4,11 @@ import sh
 
 """
 This command can show much better log, and try to write git commit as a story.
+
+##### Reference
+
+- [Your Git Log Should Tell A Story](http://www.mokacoding.com/blog/your-git-log-should-tell-a-story/?utm_campaign=CodeTengu&utm_medium=web&utm_source=CodeTengu_75)
+
 """
 
 print(sh.git.log("--format=oneline", "--abbrev-commit"))
