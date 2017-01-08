6fdd3172871b3360fe5de50f1e92360517436f09
kkchuchu <kkchuchualbert@gmail.com>
2017-01-08 20:56:10 +0800

    use os.system direct run command

--- zookeeper_script.py
+++ zookeeper_script.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 
 """
-Deploying zookeeper to Google Compute Engine
+Deploying zookeeper and kafka to Google Compute Engine
 """
 from subprocess import call
+
+# install java
+call(["apt-get", "update"])
+call(["apt-get", "-y", "install", "default-jre"])
+
 # download kafka 
 KAFKA_FILE = "kafka_2.11-0.10.1.0.tgz"
 KAFKA_LOCATION = "http://apache.stu.edu.tw/kafka/0.10.1.0/{file}".format(file=KAFKA_FILE)
 KAFKA_SERVER_CONFIG = "server.properties.kafka_2.11-0.10.1.0" 
 KAFKA_SERVER_CONFIG_LOCATION = "https://raw.githubusercontent.com/kkchuchu/kkchuchu.mushroom/master/config/{config}".format(config=KAFKA_SERVER_CONFIG)
 
-call(["wget", KAFKA_LOCATION])
+os.system("wget " + KAFKA_LOCATION)
 
 # untar package
-call(["tar", "-xzf", "kafka_2.11-0.10.1.0.tgz"])
+os.system("tar -xzf kafka_2.11-0.10.1.0.tgz")
 
-call(["ln", "-s", "kafka_2.11-0.10.1.0", "kafka"])
+os.system("ln -s kafka_2.11-0.10.1.0 kafka")
 
 # download kafka server.properties
-call(["wget", KAFKA_SERVER_CONFIG_LOCATION])
-call(["mv", KAFKA_SERVER_CONFIG, "./kafka/config/server.properties"])
-call(["./kafka/bin/zookeeper-server-stop.sh"])
-call(["./kafka/bin/zookeeper-server-start.sh", "-daemon", "./kafka/config/zookeeper.properties"])
+os.system("wget " + KAFKA_SERVER_CONFIG_LOCATION)
+os.system("mv " + KAFKA_SERVER_CONFIG + " ./kafka/config/server.properties")
+os.system("./kafka/bin/zookeeper-server-stop.sh"])
+os.system("./kafka/bin/zookeeper-server-start.sh -daemon ./kafka/config/zookeeper.properties")
 
-call(["./kafka/bin/kafka-server-stop.sh"])
-call(["./kafka/bin/kafka-server-start.sh -daemon ./kafka/config/server.properties"])
+os.system("./kafka/bin/kafka-server-stop.sh")
+os.system("./kafka/bin/kafka-server-start.sh -daemon ./kafka/config/server.properties")
