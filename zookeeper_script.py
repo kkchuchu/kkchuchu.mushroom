c3ae9f7aeeb804c3b1b06000f1147c19c22603f0
kkchuchu <kkchuchualbert@gmail.com>
2017-01-08 20:01:53 +0800

    zookeeper_script advertised.listener

--- zookeeper_script.py
+++ zookeeper_script.py
@@ -3,16 +3,24 @@
 """
 Deploying zookeeper to Google Compute Engine
 """
-
+from subprocess import call
 # download kafka 
-wget http://apache.stu.edu.tw/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz
+KAFKA_LOCATION = "http://apache.stu.edu.tw/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz"
+KAFKA_SERVER_CONFIG_LOCATION = "https://raw.githubusercontent.com/kkchuchu/kkchuchu.mushroom/master/config/server.properties.kafka_2.11-0.10.1.0"
+
+call(["wget", KAFKA_LOCATION])
 
 # untar package
-tar -xzf kafka_2.11-0.10.1.0.tgz
+call(["tar", "-xzf", "kafka_2.11-0.10.1.0.tgz"])
 
-ln -s kafka kafka_2.11-0.10.1.0
+call(["ln", "-s", "kafka", "kafka_2.11-0.10.1.0"])
 
-cd kafka
+cd kafka/config
 
-# download server.config
-wget 
+# download kafka server.properties
+call(["wget", KAFKA_SERVER_CONFIG_LOCATION])
+call(["./kafka/bin/zookeeper-server-stop.sh"])
+call(["./kafka/bin/zookeeper-server-start.sh", "--daemon", "./kafka/config/zookeeper.properties"])
+
+call(["./kafka/bin/kafka-server-stop.sh"])
+call(["./kafka/bin/kafka-server-start.sh --daemon config/server.properties"])
