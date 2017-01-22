eaa254c563ea5c00fa7f75f21e00d4aa5ed5c0f0
kkchuchu <kkchuchualbert@gmail.com>
2017-01-22 21:10:27 +0800

    use inner max outer min success in small n

--- get_money_amount.py
+++ get_money_amount.py
@@ -6,22 +6,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [0 for i in range(n)]
-        return self.r(math.ceil(float(n)/2))
+        self.db = [0 for i in range(n+1)]
+        self.r(n)
+        return self.db[n]
         
     def r(self, n):
-        if n == 1:
-            return 0
-        if len(n) == 2:
-            return 
-        return n + self.r(math.ceil(float(n)/2))
+        for i in range(2, n+1):
+            if i == 2:
+                self.db[i] = 1
+            elif i == 3:
+                self.db[3] = 2
+            else:
+                self.db[i] = min([self.db[i-2] + i-1, max(i-2 + i-1, self.db[i-3] + i-2), max(self.db[i-4] + i-3, i-3 + i-1)])
 
 
 if __name__ == '__main__':
     s = Solution()
-    assert s.getMoneyAmount(0) == 0
-    assert s.getMoneyAmount(1) == 1
-    assert s.getMoneyAmount(2) == 2
-    assert s.getMoneyAmount(3) == 4
-    assert s.getMoneyAmount(4) == 6
-    assert s.getMoneyAmount(5) == 8
+    # assert s.getMoneyAmount(0) == 0
+    # assert s.getMoneyAmount(1) == 0
+    # assert s.getMoneyAmount(2) == 1
+    # assert s.getMoneyAmount(3) == 2
+    # assert s.getMoneyAmount(4) == 4
+    assert s.getMoneyAmount(5) == 6
+    assert s.getMoneyAmount(6) == 8
+    assert s.getMoneyAmount(9) == 14
