3b8a208a35aaf03f70b0d4f100aa4107cb70b52c
kkchuchu <kkchuchualbert@gmail.com>
2018-07-22 23:22:19 +0800

    find a poker eval lib

--- get_SHOW_ACTION.py
+++ get_SHOW_ACTION.py
@@ -2,7 +2,8 @@ import json
 import sys
 import pandas as pd
 from pdb import set_trace
-
+import pokereval.card as deuces
+from pokereval.hand_evaluator import HandEvaluator
 
 def run(file_name):
     with open(file_name, 'r') as f:
@@ -44,6 +45,19 @@ def to_dict(content):
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
-    run(file_name)
-    r = pd.DataFrame.read_csv(file_name + '.csv')
+    # run(file_name)
+    r = pd.DataFrame.from_csv(file_name + '.csv')
+    print(r.head())
+    def heval(x):
+        c = eval(x['cards'])
+        hand = [deuces.Card.new(c[0][0] + c[0][1].lower()), deuces.Card.new(c[1][0] + c[1][1].lower())]
+        bs = eval(x['board'])
+        board = []
+        for b in bs:
+            t = deuces.Card.new(b[0] + b[1].lower())
+            board.append(t)
+        print(hand, board)
+        return deuces.Evaluator().evaluate(board, hand)
+
+    r['heval'] = r.apply(lambda x: heval(x), axis=1)
     print(r.head())
