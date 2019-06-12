4752b9fbc157935b518d8ae8cca5532b2aa7d3df
albert_chen <albert_chen@trend.com.tw>
2019-06-12 09:20:46 +0000

    add config

--- ktool/__init__.py
+++ ktool/__init__.py
@@ -5,8 +5,11 @@ import warnings
 from scipy.stats import norm, skew
 import seaborn as sns
 
-def ignore_warn(*args, **kwargs):
-    pass
-warnings.warn = ignore_warn
-# pd.set_option('display.expand_frame_repr', False)
-sns.set(style="whitegrid", color_codes=True)
+
+def create_notebook_env():
+    def ignore_warn(*args, **kwargs):
+        pass
+    warnings.warn = ignore_warn
+    # pd.set_option('display.expand_frame_repr', False)
+    sns.set(style="whitegrid", color_codes=True)
+    pd.options.display.max_columns = None
