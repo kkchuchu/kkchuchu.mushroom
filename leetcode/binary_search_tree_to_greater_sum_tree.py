1d5ba971747f6304fc3956d4571145a6b3ffa0f8
kkchuchu <kkchuchualbert@gmail.com>
2019-07-29 17:54:05 +0800

    update binary search tree to greater sum tree

--- leetcode/binary_search_tree_to_greater_sum_tree.py
+++ leetcode/binary_search_tree_to_greater_sum_tree.py
@@ -11,7 +11,31 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        pass
+        self.go_right_first(root, 0)
+        return root
 
-    def go_right_first(self, node):
-        pass
+    def go_right_first(self, node, right_parent_value):
+        if node.right:
+            self.go_right_first(node.right, right_parent_value)
+        self.update_value(node, right_parent_value)
+        if node.left:
+            self.go_right_first(node.left, node.val)
+
+    def update_value(self, node, right_parent_value):
+        node.val = right_parent_value + node.val
+        
+
+
+if __name__ == '__main__':
+    root = TreeNode(4)
+    root.left = TreeNode(1)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(2)
+    root.left.right.right = TreeNode(3)
+    root.right = TreeNode(6)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(7)
+    root.right.right.right = TreeNode(8)
+    sol = Solution()
+    root = sol.bstToGst(root)
+    print root.val
