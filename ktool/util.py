256c6ca0fed21852d73bb9c5a84a8a78d246b088
kkchuchu <kkchuchualbert@gmail.com>
2019-08-01 11:21:24 +0800

    add inorder visit

--- ktool/util.py
+++ ktool/util.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+
+
+
+COLORS = ['black', 'blue', 'purple', 'yellow', 'white', 'red', 'lime', 'cyan', 'orange', 'gray']
+
+def create_notebook_env():
+    import seaborn as sns
+    import pandas as pd
+    # pd.set_option('display.expand_frame_repr', False)
+    sns.set(style="whitegrid", color_codes=True)
+    pd.options.display.max_columns = None
+
+
+def check_df_before_modeling(df):
+    is_null_count = d.isnull().sum()
+    print('is null check result:')
+    print(is_null_count)
+
 def get_basic(df):
     print("=======")
     print(df.info())
@@ -11,8 +32,17 @@ def xgb_plot_features(booster, figsize=(10,14)):
     fig, ax = plt.subplots(1,1,figsize=figsize)
     return plot_importance(booster=booster, ax=ax)
 
+
 def boxplot(df, column):
     import matplotlib.pyplot as plt
     plt.figure(figsize=(10,4))
     plt.xlim(df[[column]].min(), df[['column']].max()*1.1)
     sns.boxplot(x=df[[column]])
+
+def inorder_traversal(node, r):
+    if node is None:
+        return
+    
+    inorder_traversal(node.left, r)
+    r.append(node.val)
+    inorder_traversal(node.right, r)
