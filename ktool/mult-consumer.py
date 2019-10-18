d88b4947863278d530ddbcdd4383b80f9d9e26a9
kkchuchu <kkchuchualbert@gmail.com>
2019-10-18 17:17:22 +0800

    update tool

--- ktool/mult-consumer.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-
-print subprocess.call("python ~/Downloads/workspace/pcer/pcer/fm.py consumer", shell=True)
-print subprocess.call("python ~/Downloads/workspace/pcer/pcer/fm.py consumer", shell=True)
