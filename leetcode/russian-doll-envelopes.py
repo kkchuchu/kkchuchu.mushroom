652293c9868edd75206b86705d36677e23f83f55
kkchuchu <kkchuchualbert@gmail.com>
2019-11-21 16:01:21 +0800

    update notebook

--- leetcode/russian-doll-envelopes.py
+++ leetcode/russian-doll-envelopes.py
@@ -4,30 +4,38 @@ class Solution(object):
         if l == 0:
             return l
         envelops[0].append(1)
-        tmp, index, stop = [envelops[0]], 0, False
+        ordered = [envelops[0]]
         for envelop in envelops[1:]:
-            for i, i_envelop in enumerate(tmp):
-                if envelop[0] > i_envelop[0]:
-                    continue
-                elif envelop[1] > i_envelop[1]:
-                    continue
-                elif envelop[0] == i_envelop[0] and envelop[1] == i_envelop[1]:
-                    break
+            max_val = ordered[0][2]
+            position = 0
+            for i, o_envelop in enumerate(ordered):
+                if envelop[0] > o_envelop[0] or envelop[1] > o_envelop[1]:
+                    position += 1
+                    if envelop[0] > o_envelop[0] and envelop[1] > o_envelop[1] \
+                            and max_val <= o_envelop[2]:
+                        max_val += 1
                 else:
-                    if not stop:
-                        index = i
-                        stop = True
-                    if envelop[0] < i_envelop[0] and \
-                            envelop[1] < i_envelop[1]:
-                        i_envelop[2] = i_envelop[2] + 1
+                    break
 
-            envelop.append(index)
-            tmp.insert(index, envelop)
-            print(tmp, envelop, index)
+            envelop.append(max_val)
+            ordered.insert(position, envelop)
+            for o_envelop in ordered[position+1:]:
+                if envelop[0] < o_envelop[0] and \
+                        envelop[1] < o_envelop[1]:
+                    o_envelop[2] = o_envelop[2] + 1
+                print(o_envelop[2])
+                if envelop[0] == o_envelop[0] and \
+                        envelop[1] == o_envelop[1]:
+                    break
+        from pdb import set_trace; set_trace()
 
-        return max(tmp, key=lambda x: x[2])[2]
+        return max(ordered, key=lambda x: x[2])[2]
 
 
 if __name__ == '__main__':
-    print(Solution().maxEnvelopes([[5, 4], [6, 4], [6, 7], [2, 3]]))
-    print(Solution().maxEnvelopes([[1,1], [1,1], [1,1]]))
+    # assert 3 == Solution().maxEnvelopes([[5, 4], [6, 4], [6, 7], [2, 3]])
+    # assert 1 == Solution().maxEnvelopes([[1,1], [1,1], [1,1]])
+    # assert 3 == Solution().maxEnvelopes([[4,5],[6,7],[2,3]])
+    # assert 4 == Solution().maxEnvelopes([[4,5],[4,6],[6,7],[2,3],[1,1]])
+    # assert 4 == Solution().maxEnvelopes([[4,5],[4,6],[6,7],[2,3],[1,1],[1,1]])
+    assert 3 == Solution().maxEnvelopes([[30,50],[12,2],[3,4],[12,15]])
