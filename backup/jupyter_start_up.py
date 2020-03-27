606cb4f5bedadc4b0c844614fb5880e729808c7f
albert.chen <albert.chen@telmedia.com>
2020-03-27 11:14:14 +0800

    add %load startup script

---
+++ backup/jupyter_start_up.py
@@ -0,0 +1,34 @@
+%load_ext autoreload
+%autoreload 2
+%matplotlib inline
+%precision 4
+%reload_ext autoreload
+# Load the TensorBoard notebook extension.
+%load_ext tensorboard
+
+import re
+import sys
+import math
+import random
+import os
+from datetime import datetime
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+import warnings
+warnings.filterwarnings(action='once')
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
+pd.options.display.max_rows = None
+
+import seaborn as sns
+sns.set(style="whitegrid")
+
+import ktool
