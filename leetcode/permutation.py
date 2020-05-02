6632694b3138ec2f699fbeb6987555f806618394
Yi-Chung Chen <kkchuchualbert@gmail.com>
2020-05-03 02:06:01 +0800

    Update permutation.py

--- leetcode/permutation.py
+++ leetcode/permutation.py
@@ -29,3 +29,48 @@ n = len(string)
 a = list(string) 
 permute(a, 0, n-1) 
 
+
+### product
+
+
+def product(pool, r=None):
+    #result = [[]]
+    #for i in range(r):
+    #    result = [x+[y] for x in result for y in pool]
+    result = [a for a in pool]
+    for i in range(r-1):
+        tmp = []
+        for x in result:
+            for a in pool:
+                tmp.append(x+a) # change to yield
+        result = tmp
+            
+        
+    print(result, len(result))
+
+product('ABC', r=3)
+
+
+def c_product(*args, r=None):
+    result = [[x] for x in args[0]]
+    for arg in args[1:]:
+        tmp = []
+        for x in result:
+            for y in arg:
+                tmp.append(x+[y])
+        result = tmp
+        
+    print(result, len(result))
+
+c_product('ABC', "DE", "FG")
+
+
+def permutation(pool, r=None):
+    result = product("ABC", r=r)
+    p_result = []
+    for x in result:
+        if len(list(set(x))) == r:
+            p_result.append(x)
+    print(p_result, len(p_result))
+    
+permutation("ABC", r=2)
